46c0ceb064cb8e72e25848bb14d4a15ad9e5c952
Naturalis007 <tamara.salasvergara@gmail.com>
2023-03-08 03:44:19 +0000

    simple counter creado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,22 +3,22 @@ import React from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
+
 //create your first component
-const Home = () => {
+const Home = ({segundos}) => {
+	let string = segundos.toString();
+	let unidad = string[string.length -1]
+	let decena = string[string.length -2]
+	let centena = string[string.length -3]
+	let mil = string[string.length -4]
+
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<button className="btn btn-primary"><i className="fa-solid fa-clock"></i></button>
+			<button className="btn btn-primary">{mil===undefined? "0": mil}</button>
+			<button className="btn btn-primary">{centena===undefined? "0": centena}</button>
+			<button className="btn btn-primary">{decena===undefined? "0": decena}</button>
+			<button className="btn btn-primary">{unidad===undefined? "0": unidad}</button>
 		</div>
 	);
 };
--- src/js/index.js
+++ src/js/index.js
@@ -8,5 +8,12 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let tiempo = 0;
+
+setInterval (()=>{
+    //render your react application
+    ReactDOM.render(<Home segundos={tiempo} />, document.querySelector("#app"));
+    tiempo++
+},1000)
+
+
